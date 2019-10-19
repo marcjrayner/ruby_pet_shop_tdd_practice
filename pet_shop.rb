60d61ad5ee944421f8144c27425493f9a297d674
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-10-19 09:02:59 +0100

    MVP complete

--- pet_shop.rb
+++ pet_shop.rb
@@ -64,3 +64,11 @@ end
 def remove_customer_cash(customer, amount)
   customer[:cash] -= amount
 end
+
+def customer_pet_count(customer)
+  return customer[:pets].count
+end
+
+def add_pet_to_customer(customer, pet)
+  customer[:pets].push(pet)
+end
